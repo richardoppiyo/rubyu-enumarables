2d12e510c8d6750737f68ee15c12789048b72b8b
Thi-Ponce <thi.pponce@gmail.com>
2022-05-06 13:12:49 -0300

    fix linters

--- list.rb
+++ list.rb
@@ -5,20 +5,20 @@ class MyList
   def initialize(*params)
     @list = params
   end
+
   def each(&block)
     @list.each(&block)
   end
 end
 
-
 list = MyList.new(1, 2, 3, 4)
 
-puts list.all? {|e| e < 5}
+puts (list.all? { |e| e < 5 })
 
-puts list.all? {|e| e > 5}
+puts (list.all? { |e| e > 5 })
 
-puts list.any? {|e| e == 2}
+puts (list.any? { |e| e == 2 })
 
-puts list.any? {|e| e == 5}
+puts (list.any? { |e| e == 5 })
 
-print list.filter {|e| e.even?}
+print (list.filter { |e| e.even? })
